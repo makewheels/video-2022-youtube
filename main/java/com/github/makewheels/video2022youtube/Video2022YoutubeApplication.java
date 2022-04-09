b9a4ba0a4bdc87c42c37a28950628f3bfc32e1b4
makewheels <makewheels@github.com>
2022-04-09 14:59:11 +0800

    init

---
+++ main/java/com/github/makewheels/video2022youtube/Video2022YoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.video2022youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Video2022YoutubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Video2022YoutubeApplication.class, args);
+    }
+
+}
---
+++ test/java/com/github/makewheels/video2022youtube/Video2022YoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.video2022youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Video2022YoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
