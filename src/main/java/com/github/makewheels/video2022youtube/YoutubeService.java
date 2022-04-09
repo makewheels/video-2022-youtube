8030ab6b3e99a5d356d0586b95d740bfa49364f0
makewheels <makewheels@github.com>
2022-04-09 16:52:36 +0800

    有个问题啊，国内服务器是不是要获取YouTube视频信息？

--- src/main/java/com/github/makewheels/video2022youtube/YoutubeService.java
+++ src/main/java/com/github/makewheels/video2022youtube/YoutubeService.java
@@ -1,6 +1,7 @@
 package com.github.makewheels.video2022youtube;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.RuntimeUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +13,8 @@ import java.io.*;
 @Service
 @Slf4j
 public class YoutubeService {
-    @Value("${workDir}")
-    private String workDir;
+    @Value("${youtube-work-dir}")
+    private String youtubeWorkDir;
 
     public void executeAndPrint(String cmd) {
         try {
@@ -29,16 +30,14 @@ public class YoutubeService {
         }
     }
 
-    /**
-     * 提交新视频任务
-     */
-    public JSONObject submitMission(String youtubeUrl) {
+    public void download(String youtubeUrl) {
         new Thread(() -> {
             //获取视频文件信息
             String getFilenameCmd = "yt-dlp --get-filename -o '%(title)s.%(ext)s' "
                     + "--restrict-filenames " + youtubeUrl;
             log.info("getFilenameCmd = " + getFilenameCmd);
             String filename = RuntimeUtil.execForStr(getFilenameCmd);
+            log.info("filename before = " + filename);
             filename = filename.replace("'", "");
             filename = filename.replace("+", "_");
             filename = filename.replace("%", "_");
@@ -59,13 +58,14 @@ public class YoutubeService {
             filename = filename.replace(" ", "_");
             filename = filename.replace("\n", "");
             filename = filename.replace("\r", "");
-            log.info("filename = " + filename);
+            log.info("filename after = " + filename);
 
+            String workId = IdUtil.randomUUID();
             //下载视频
-            File webmFile = new File(workDir, "videoId" + "/download/" + filename);
-            System.out.println("webmFile = " + webmFile);
+            File webmFile = new File(youtubeWorkDir, workId + "/" + filename);
+            log.info("webmFile = " + webmFile);
             String downloadCmd = "yt-dlp -S height:1080 -o " + webmFile.getAbsolutePath() + " " + youtubeUrl;
-            System.out.println("downloadCmd = " + downloadCmd);
+            log.info("downloadCmd = " + downloadCmd);
             executeAndPrint(downloadCmd);
 
             //上传对象存储
@@ -74,29 +74,38 @@ public class YoutubeService {
 
 //            String base = BaiduCloudUtil.getObjectStoragePrefix(videoId);
 //            String key = base + videoId + "." + FilenameUtils.getExtension(webmFile.getName());
-//            System.out.println("key = " + key);
+//            log.info("key = " + key);
 //            String playFileUrl = BaiduCloudUtil.uploadObjectStorage(webmFile, key);
 
-            System.out.println("上传对象存储完成");
-//            System.out.println("playFileUrl = " + playFileUrl);
+            log.info("上传对象存储完成");
+//            log.info("playFileUrl = " + playFileUrl);
             long end = System.currentTimeMillis();
             long time = end - start;
             long fileLength = webmFile.length();
             long speedPerSecond = fileLength / time * 1000;
             String readable = FileUtil.readableFileSize(speedPerSecond);
-            System.out.println("上传对象存储速度:  " + readable + "/s");
+            log.info("上传对象存储速度:  " + readable + "/s");
 
             //通知
-            System.out.println("通知国内服务器");
+            log.info("通知国内服务器");
 
             //删除本地下载的文件
-//            System.out.println("FileUtil.del(new File(workDir, videoId)) = "
+//            log.info("FileUtil.del(new File(workDir, videoId)) = "
 //                    + FileUtil.del(new File(workDir, videoId)));
 
         }).start();
+    }
+
+    /**
+     * 提交新视频任务
+     */
+    public JSONObject submitMission(String youtubeUrl) {
+        download(youtubeUrl);
 
         //提前先返回播放地址
-        return null;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("youtubeUrl", youtubeUrl);
+        return jsonObject;
     }
 
 }
