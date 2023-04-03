8c8ba065f2752fcc4ea741efda52ab87fb3caf55
makewheels <33256842+makewheels@users.noreply.github.com>
2023-04-03 21:03:06 +0800

    日志打印健康检查

--- src/main/java/com/github/makewheels/video2022youtube/HealthCheckController.java
+++ src/main/java/com/github/makewheels/video2022youtube/HealthCheckController.java
@@ -1,12 +1,15 @@
 package com.github.makewheels.video2022youtube;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthCheckController {
     @GetMapping("healthCheck")
     public String healthCheck() {
+        log.info("healthCheck" + System.currentTimeMillis());
         return "ok " + System.currentTimeMillis();
     }
 }
